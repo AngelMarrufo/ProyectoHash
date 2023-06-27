59790b4aa2368ebad21a39ff37e0e5c40ff90334
Angel <Angelmarrufo79@gmail.com>
2023-06-27 16:25:20 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoHash/Hash.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHash
+{
+    internal class Hash
+    {
+        private const int TableSize = 10;
+        private LinkedList<KeyValue>[] table;
+
+        public Hash()
+        {
+            table = new LinkedList<KeyValue>[TableSize];
+        }
+
+        public void Add(string key, string value)
+        {
+            int index = HashFunction(key);
+
+            if (table[index] == null)
+            {
+                table[index] = new LinkedList<KeyValue>();
+            }
+
+            foreach (KeyValue kv in table[index])
+            {
+                if (kv.Key == key)
+                {
+                    kv.Value = value;
+                    return;
+                }
+            }
+
+            table[index].AddLast(new KeyValue(key, value));
+        }
+
+        public string Get(string key)
+        {
+            int index = HashFunction(key);
+
+            if (table[index] != null)
+            {
+                foreach (KeyValue kv in table[index])
+                {
+                    if (kv.Key == key)
+                    {
+                        return kv.Value;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private int HashFunction(string key)
+        {
+            int hash = 0;
+
+            foreach (char c in key)
+            {
+                hash += (int)c;
+            }
+
+            return hash % TableSize;
+        }
+
+        private class KeyValue
+        {
+            public string Key { get; }
+            public string Value { get; set; }
+
+            public KeyValue(string key, string value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+}
---
+++ ProyectoHash/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hash hashTable = new Hash();
+
+            hashTable.Add("key1", "value1");
+            hashTable.Add("key2", "value2");
+            hashTable.Add("key3", "value3");
+            hashTable.Add("key4", "value4");
+            hashTable.Add("key5", "value5");
+            hashTable.Add("key6", "value6");
+            hashTable.Add("key7", "value7");
+
+            Console.WriteLine("Valor de 'key1': " + hashTable.Get("key1"));
+            Console.WriteLine("Valor de 'key3': " + hashTable.Get("key3"));
+            Console.WriteLine("Valor de 'key6': " + hashTable.Get("key6"));
+            Console.WriteLine("Valor de 'key8': " + hashTable.Get("key8"));
+
+            Console.ReadLine();
+        }
+    }
+}
